206c6254aeed06f89dd7b34e06ddd8c98f8ed736
OneGrit <2012joe@gmail.com>
2019-04-03 16:17:39 +0800

    new URL + view for adding to existing lists. FT passes :-)

--- lists/tests.py
+++ lists/tests.py
@@ -58,21 +58,36 @@ class ListAndItemModelTest(TestCase):
 
 
 class ListViewTest(TestCase):
-    def test_displays_all_items(self):
-        """UT：在表格中显示多个待办事项"""
-        a_list = List.objects.create()
-        Item.objects.create(text='First item to do', list=a_list)
-        Item.objects.create(text='Second item to do', list=a_list)
+    def test_displays_only_items_for_that_list(self):
+        """UT：相互独立的待办事项清单列表"""
+        correct_list = List.objects.create()
+        Item.objects.create(text='First item to do', list=correct_list)
+        Item.objects.create(text='Second item to do', list=correct_list)
 
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        other_list = List.objects.create()
+        Item.objects.create(text='Other  item to do 1', list=other_list)
+        Item.objects.create(text='Other  item to do 2', list=other_list)
+
+        response = self.client.get(f'/lists/{correct_list.id}/')
 
         self.assertContains(response, 'First item to do')
         self.assertContains(response, 'Second item to do')
+        self.assertNotContains(response, 'Other  item to do 1')
+        self.assertNotContains(response, 'Other  item to do 2')
 
     def test_use_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        a_list = List.objects.create()
+        response = self.client.get(f'/lists/{a_list.id}/')
 
         self.assertTemplateUsed(response, 'list.html')
+
+    def test_passes_correct_list_to_template(self):
+        """列表视图向模板传递正确的清单id"""
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.get(f'/lists/{correct_list.id}/')
+        self.assertEqual(response.context['list'], correct_list)
 
 
 class NewListTest(TestCase):
@@ -92,6 +107,7 @@ class NewListTest(TestCase):
     def test_redirects_after_post(self):
         """UT:POST请求后应该重定向到list页面"""
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})  # 此处的item_text是模板中input的name
+        new_list = List.objects.first()
 
         # 重定向到列表页面
         # 断言：response的状态码
@@ -99,4 +115,30 @@ class NewListTest(TestCase):
         # 断言：response的location
         # self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
         # 重构：使用assertRedirect重构上面的代码
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        self.assertRedirects(response, f'/lists/{new_list.id}/')
+
+
+class NewItemTest(TestCase):
+    def test_can_save_a_post_request_to_an_existing_list(self):
+        """UT:新的待办事项能够添加到已存在清单中"""
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(f'/lists/{correct_list.id}/add_item',
+                         data={'item_text': 'A new item for an existing list'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        """POST后重定向到新的列表视图"""
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f'/lists/{correct_list.id}/add_item',
+            data={'item_text': 'A new item for an existing list'}
+        )
+        self.assertRedirects(response, f'/lists/{correct_list.id}/')
--- lists/views.py
+++ lists/views.py
@@ -22,12 +22,19 @@ def home_page(request: HttpRequest):
     return render(request, 'home.html')
 
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+def view_list(request, list_id):
+    a_list = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': a_list})
 
 
 def new_list(request):
     a_list = List.objects.create()
     Item.objects.create(text=request.POST['item_text'], list=a_list)
-    return redirect('/lists/the-only-list-in-the-world/')
+    return redirect(f'/lists/{a_list.id}/')
+
+
+def add_item(request, list_id, ):
+    a_list = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=a_list)
+
+    return redirect(f'/lists/{a_list.id}/')
