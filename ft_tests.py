2283d0d88c0ed230866a47fbe8a0edc5c74d6c24
OneGrit <2012joe@gmail.com>
2019-04-03 08:45:42 +0800

    重构FT，新增check_for_row_in_list_table方法，替换功能测试中重复代码

--- ft_tests.py
+++ ft_tests.py
@@ -13,6 +13,12 @@ class NewVisitorTest(unittest.TestCase):  # 测试组织成类的形式，继承
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """FT:用户使用待办事项功能"""
         # Edith 听说有一个很酷的在线待办事项应用
@@ -35,16 +41,17 @@ class NewVisitorTest(unittest.TestCase):  # 测试组织成类的形式，继承
         input_box.send_keys(keys.Keys.ENTER)
         time.sleep(1)
         # 在待办事项表格中显示了“1: Buy peacock feathers”
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # 使用辅助方法重构上面两行
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
         # #判断内容是否在表格的多个行中
         # self.assertTrue(
         #     any(row.text == "1: Buy peacock feathers" for row in rows),
         #     f"New to-do item did not appear in table. Contents were:\n{table.text}"  # assert×添加失败的消息
         # )
         # 重构上面的代码，修改为如下：
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        # self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
 
         # 页面中又显示了一个文本框，可以输入其他待办事项
         input_box = self.browser.find_element_by_id('id_new_item')
@@ -55,10 +62,13 @@ class NewVisitorTest(unittest.TestCase):  # 测试组织成类的形式，继承
         # Edith做事很有条理
 
         # 页面再次更新，在她的待办事项列表中显示了两个待办事项
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
-        self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows])
+        # table = self.browser.find_element_by_id('id_list_table')
+        # rows = table.find_elements_by_tag_name('tr')
+        # self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        # self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows])
+        # 使用辅助方法重构上面4行
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
         self.fail('Finish the test!')
 
