1daa1521f9f7e7889158a4a2d9aa5be0a335e8c1
OneGrit <2012joe@gmail.com>
2019-04-03 08:35:03 +0800

    功能测试添加多次添加待办事项测试

--- ft_tests.py
+++ ft_tests.py
@@ -23,13 +23,13 @@ class NewVisitorTest(unittest.TestCase):  # 测试组织成类的形式，继承
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        # 她在文本框中输入了“Buy peacock feathers” （购买孔雀羽毛）
-        # 伊迪丝的爱好是使用假蝇做饵钓鱼
         input_box = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             input_box.get_attribute('placeholder'),
             'Enter a to-do item'
         )
+        # 她在文本框中输入了“Buy peacock feathers” （购买孔雀羽毛）
+        # 伊迪丝的爱好是使用假蝇做饵钓鱼
         input_box.send_keys('Buy peacock feathers')
         # 她按回车键后，页面更新了
         input_box.send_keys(keys.Keys.ENTER)
@@ -38,18 +38,29 @@ class NewVisitorTest(unittest.TestCase):  # 测试组织成类的形式，继承
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
 
-        self.assertTrue(
-            any(row.text == "1: Buy peacock feathers" for row in rows),
-            "New to-do item did not appear in table"  # assert×添加失败的消息
-        )
-
-        self.fail('Finish the test!')
+        # #判断内容是否在表格的多个行中
+        # self.assertTrue(
+        #     any(row.text == "1: Buy peacock feathers" for row in rows),
+        #     f"New to-do item did not appear in table. Contents were:\n{table.text}"  # assert×添加失败的消息
+        # )
+        # 重构上面的代码，修改为如下：
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
 
         # 页面中又显示了一个文本框，可以输入其他待办事项
+        input_box = self.browser.find_element_by_id('id_new_item')
         # 她输入了“Use peacock feathers to make a fly” （使用孔雀羽毛做假蝇）
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(keys.Keys.ENTER)
+        time.sleep(1)
         # Edith做事很有条理
 
         # 页面再次更新，在她的待办事项列表中显示了两个待办事项
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        self.assertIn("2: Use peacock feathers to make a fly", [row.text for row in rows])
+
+        self.fail('Finish the test!')
 
         # Edith想知道这个网站是否会记住她的清单（待办事项清单）
         # 她看到网站为她生成了一个唯一的URL
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,15 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
-def home_page(request):
-    return render(request, 'home.html')
+def home_page(request: HttpRequest):
+    # 处理POST请求
+    # if request.method == 'POST':
+    #     return render(request, 'home.html', {
+    #         'new_item_text': request.POST['item_text']
+    #     })
+    return render(request, 'home.html', {
+        'new_item_text': request.POST.get('item_text','')
+    })
+
+    # return render(request, 'home.html')
